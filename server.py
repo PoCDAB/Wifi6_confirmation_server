3e60849f606605f62e5651195829fc848cab5e15
FrankMonteny8 <frankmonteny02@gmail.com>
2021-11-09 16:13:29 +0100

    bug fix if else expression

--- server.py
+++ server.py
@@ -109,7 +109,7 @@ def client_thread(conn, addr):
 def store_confirmation(confirmation):
     dab_confirmation = DAB_confirmation(confirmation.get("dab_id"), confirmation.get("message_type"), confirmation.get("dab_message_arrived_at"))
 
-    DAB_confirmations.append(dab_confirmation) if not dab_confirmation.dab_id in [confirmation.dab_id for confirmation in DAB_confirmations] else return
+    DAB_confirmations.append(dab_confirmation) if not dab_confirmation.dab_id in [confirmation.dab_id for confirmation in DAB_confirmations] else print("[SERVER] DAB_confirmation already in list")
 
 """
     This function shows all the DAB confirmations after it sorted the list on dab_id
